bea86897e5ab0cee8a395626bfeb34cea99153bd
Tippu Fisal <tippufisal@vajro.com>
2021-05-01 20:14:44 +0530

    *Datatypes Program

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+
+    // Data types
+    val number:Int = 234
+    val bigNumber:Long = 233232332
+    val decimalValue:Float = 23.3F // we can use small f or F
+    val bigDecimalValue:Double = 343.564
+    val character:Char = 'T'
+    val courseName:String = "Kotlin Tutorial"
+    val isAlive:Boolean = true // in this datatype we can use true or false
+
+    println("Int: $number")
+    println("Long: $bigNumber")
+    println("Float: $decimalValue")
+    println("Double: $bigDecimalValue")
+    println("Char: $character")
+    println("String: $courseName")
+    println("Boolean: $isAlive")
+}
