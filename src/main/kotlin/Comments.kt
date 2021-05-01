a11ef8879022c8a3a59f74c7c7786df5ca914319
Tippu Fisal <tippufisal@vajro.com>
2021-05-01 18:45:34 +0530

    *Comments Program

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,16 @@
+
+
+// This is inline or single line comments
+
+/**
+ * Multi line comments example :
+ *  if we use // or /**/ your code will not consider as code, take it as comments
+ *
+ *  Multi line Comments 1
+ *  Multi line Comments 2
+ */
+
+
+fun main(args: Array<String>){
+    println("This is comments program")
+}
